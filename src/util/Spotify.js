921b1b00a0222ad060e126e466c5887753dedd59
Simon Hutchinson <simonhutc@gmail.com>
2018-03-03 18:48:48 +1100

    redirect

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,7 @@
 const clientID = "25b4bc3aa4924ed7bb4fb19027a073b1";
 let spotifytkn = "";
-const redirect_uri = "http://simonjammin.surge.sh";
+const redirect_uri = "http://localhost:3000";
+// redirect_uri for surge deployment is http://simonjammin.surge.sh
 
 
 
